df68af85f6e2c0dfd59e2dfa6a38e74a886586d4
Angel-dash <njlghmr@gmail.com>
2023-06-14 09:50:16 +0545

    Basic UI design with navigation

---
+++ Cinemate/src/pages/MovieList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MovieList = () => {
+  return (
+    <main>
+        <div className="text-9xl bg-emerald-200 p-10 border-8 border-blue-700 rounded-lg m-5">
+        MovieList
+        </div>
+    </main>
+  )
+}
+     
